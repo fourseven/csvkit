4a2b454ba68885d5d6a8d4b9ea34521a78615754
Mathew Hartley <matt@route66.sytes.net>
2012-12-21 14:47:30 +1300

    Clean up string processing, and move to tsv output

--- lib/csvkit/csvkit.rb
+++ lib/csvkit/csvkit.rb
@@ -1,5 +1,6 @@
 class CSVKit
   require 'nokogiri'
+  require 'iconv'
 
   attr_accessor :content
 
@@ -15,17 +16,35 @@ class CSVKit
 
     doc = Nokogiri::HTML(@content)
 
-    result = ""
-
-    doc.xpath('//table//tr').each do |row|
-      row.xpath('td').each do |cell|
-        result += '"' + cell.text.gsub("\n", ' ').gsub('"', '\"').gsub(/(\s){2,}/m, '\1').gsub(/[\,\$]/, '') + "\", "
+    tsv_str = CSV.generate(col_sep: "\t", headers: :first_row, encoding: 'utf-8') do |tsv|
+      doc.xpath('//table//tr').each do |row|
+        tsv_row = []
+        row.xpath('td | th').each do |cell|
+          tsv_row << clean_cell_string(cell.text)
+        end
+        tsv << tsv_row
       end
-      result += "\n"
     end
 
-    raise "command failed: #{invoke}" if result.to_s.strip.empty?
-    return result
+    raise "command failed" if tsv_str.to_s.strip.empty?
+
+    write_content = Iconv.conv("utf-16le", "utf-8", "\xEF\xBB\xBF")
+    write_content += Iconv.conv("utf-16le", "utf-8", tsv_str)
+    write_content
+    return write_content
   end
 
+  def clean_cell_string(cell_string)
+    cell_string = cell_string.gsub(/[[:space:]]/, ' ')
+    cell_string = cell_string.gsub("\n", ' ')
+    cell_string = cell_string.gsub('"', '\"')
+    cell_string = cell_string.gsub(/(\s){2,}/m, '\1')
+    cell_string = cell_string.gsub(/[\,\$]/, '')
+    is_numeric?(cell_string) ? cell_string.to_b : cell_string.strip
+  end
+
+  def is_numeric?(string)
+    return true if self =~ /^\d+$/
+    true if Float(self) rescue false
+  end
 end
